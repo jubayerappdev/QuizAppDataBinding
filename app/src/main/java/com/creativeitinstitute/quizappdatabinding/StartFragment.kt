8c511f16f5a9b890371e0977e727e4a9062d8921
jubo445 <juboraj335.ja@gmail.com>
2025-03-08 16:20:03 +0600

    popUpTo added

--- app/src/main/java/com/creativeitinstitute/quizappdatabinding/StartFragment.kt
+++ app/src/main/java/com/creativeitinstitute/quizappdatabinding/StartFragment.kt
@@ -5,19 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.creativeitinstitute.quizappdatabinding.databinding.FragmentStartBinding
 
 
 class StartFragment : Fragment() {
 
 
+    lateinit var binding: FragmentStartBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        // Inflate the layout for this fragment\
+
+        binding = FragmentStartBinding.inflate(layoutInflater, container, false)
+
+
+        binding.playButton.setOnClickListener {
+
+            findNavController().navigate(StartFragmentDirections.actionStartFragmentToGameFragment())
+
+        }
+        return binding.root
     }
 
 
